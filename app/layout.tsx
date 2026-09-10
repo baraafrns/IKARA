af690fdd95ef54d0b7b66889125eea967d212992
barafrns <fartoofarr@gmail.com>
2026-09-10 21:01:03 +0700

    Update metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,17 +15,17 @@ const dmSans = DM_Sans({
 });
 
 export const metadata: Metadata = {
-  title: 'IKARA COMPETITION DATA CENTER',
-  description: 'Sistem Registrasi Ulang & Penjemputan Peserta Lomba Anak Masjid - IKARA Competition Data Center',
+  title: 'IKARA FESTIVAL DATA CENTER',
+  description: 'Sistem Registrasi Ulang & Penjemputan Peserta Lomba Anak Masjid - IKARA Festival Data Center',
   openGraph: {
-    title: 'IKARA COMPETITION DATA CENTER',
-    description: 'Sistem Registrasi Ulang & Penjemputan Peserta Lomba Anak Masjid - IKARA Competition Data Center',
+    title: 'IKARA FESTIVAL DATA CENTER',
+    description: 'Sistem Registrasi Ulang & Penjemputan Peserta Lomba Anak Masjid - IKARA Festival Data Center',
     type: 'website',
   },
   twitter: {
     card: 'summary_large_image',
-    title: 'IKARA COMPETITION DATA CENTER',
-    description: 'Sistem Registrasi Ulang & Penjemputan Peserta Lomba Anak Masjid - IKARA Competition Data Center',
+    title: 'IKARA FESTIVAL DATA CENTER',
+    description: 'Sistem Registrasi Ulang & Penjemputan Peserta Lomba Anak Masjid - IKARA Festival Data Center',
   },
 };
 
